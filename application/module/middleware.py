ba3cc98ab1e01b0f28714f629cb2669a3f19dd46
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2016-01-15 02:51:06 +0900

    認証処理を改修

--- application/module/middleware.py
+++ application/module/middleware.py
@@ -6,5 +6,9 @@ from django.shortcuts import redirect
 class AuthenticationMiddleware(object):
 
     def process_request(self, request):
-        if request.path == '/' and not request.user.is_authenticated():
-            return redirect('login')
+        if request.user.is_authenticated():
+            if request.path.startswith('/login'):
+                return redirect('/')
+        else:
+            if not request.path.startswith('/login'):
+                return redirect('login')
