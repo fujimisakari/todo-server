e9c2bfc536cd7c23f1c0148917c4dcb5dc6811ac
Fujimisakari <fujimisakari@gmail.com>
2017-01-16 10:54:59 +0900

    Modify init.sh

--- env/todo_init.sh
+++ env/todo_init.sh
@@ -6,10 +6,6 @@ initialize () {
         pip install -r env/requirements.txt
     fi
 
-    echo "===== DB Setup ====="
-    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
-    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
-
     python application/manage.py syncdb --noinput
     python application/manage.py migrate
 }
@@ -25,12 +21,22 @@ fi
 # For Develop
 if [ $1 -a $1 = "develop" ]; then
     source .env.develop
+
+    echo "===== DB Setup ====="
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+
     initialize $2
 fi
 
 # For Local
 if [ $1 -a $1 = "local" ]; then
     source .env.local
+
+    echo "===== DB Setup ====="
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS -h $DB_HOST
+
     initialize $2
 fi
 
